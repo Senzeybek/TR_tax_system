75028c35371f15c5e62ae38091dc608dbef5c579
Senzeybek <30653198+Senzeybek@users.noreply.github.com>
2020-09-28 21:38:29 +0200

    kredi faiz indirimi yontemi degistirildi

--- 5_kredi_indirimi.R
+++ 5_kredi_indirimi.R
@@ -1,6 +1,6 @@
 # kredi sistemi
 
-data$kullanilan_kredi <- data$yeni_fiyat*kredi_orani
+data$kullanilan_kredi <- data$hurda_tesvikli_fiyat*kredi_orani
 
 data <- data %>% mutate(avaliable_indirimli_kredi=case_when(
   uretim=="yerli" & kullanilan_kredi<  max_indirimli_kredi_miktari ~ kullanilan_kredi              ,
@@ -28,8 +28,14 @@ data$yillik_faiz_farki <- data$mevcut_yillik_faiz_odemesi-data$indirimli_yillik_
 
 
 # kredi indirimleri ile gelen satis fiyati
-data$kredi_indirimli_satis <- ifelse(data$yillik_faiz_farki>0,
-  round(data$hurda_tesvikli_satis_miktari*(1+ (mevcut_yillik_faiz- indirimli_yillik_faiz)*kredi_esnekligi)),
-  data$hurda_tesvikli_satis_miktari)
+data <- data %>% mutate(yuzde_kredi_maliyeti_degisimi= (indirimli_kredi_odemesi-mevcut_toplam_kredi_odemesi)/
+                  (hurda_tesvikli_fiyat+mevcut_toplam_kredi_odemesi),
+                kredi_indirimli_satis = round(hurda_tesvikli_satis_miktari*(1+(yuzde_kredi_maliyeti_degisimi*kendi_esnekligi))))
+
+
+
+# data$kredi_indirimli_satis <- ifelse(data$yillik_faiz_farki>0,
+#   round(data$hurda_tesvikli_satis_miktari*(1+ (mevcut_yillik_faiz- indirimli_yillik_faiz)*kredi_esnekligi)),
+#   data$hurda_tesvikli_satis_miktari)
 
 
